abc39554d60e4e20ff39b0b528b50c1746bfe4d0
Miracle-Go-123 <entsav.go@yahoo.com>
2015-09-11 18:36:33 -0700

    login button and auth form showing

---
+++ app/assets/javascripts/angular/directives.js
@@ -0,0 +1,15 @@
+coffeeBankApp.directive('dhAuth', function() {
+  return {
+    
+  	restrict: 'E',
+  	templateUrl: 'auth-form.html',
+    scope: {
+    
+      },
+
+    link: function(scope, element, attrs) {
+      
+
+    }
+  };
+});
