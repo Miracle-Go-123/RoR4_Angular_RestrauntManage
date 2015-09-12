820cd979db2df677b51e27318c92f69c20600cc0
Dovelive <dovelive.lh@hotmail.com>
2015-09-11 18:36:33 -0700

    login button and auth form showing

--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -1,11 +1,13 @@
+coffeeBankApp.controller("MainController", ["$scope", "$http", function($scope, $http) {
+
+  $scope.loginForm = false;
+
+
+}]);
+
 coffeeBankApp.controller("ItemsController", ["$scope", "$http", function($scope, $http) {
 
-
-
   $scope.test = "Yo yo home slice!";
-
-
-
 
 
 }]);
