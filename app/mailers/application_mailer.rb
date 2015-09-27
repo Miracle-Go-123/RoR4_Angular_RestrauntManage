bce182b4fabbb611d82009eb399ee563af93b9b3
Dovelive <dovelive.lh@hotmail.com>
2015-09-27 15:28:51 -0700

    class

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "stash@stashit.com"
+  # default_url_options[:host] = "localhost:3000"
   default_url_options[:host] = "https://stash-it-daily.herokuapp.com/"
 end
